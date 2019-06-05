20454d0fb949fef3e297bb4002065d828d8d7e6b
baiy <wo@baiy.org>
2019-06-05 21:20:38 +0800

    添加版本号

--- vue.config.js
+++ vue.config.js
@@ -4,11 +4,12 @@ const config = {
         index: {
             entry: 'src/main.js',
             template: 'src/index.html',
+            stat: "https://www.baiy.org/chrome_tool/stat.php?version=" + process.env.npm_package_version
         },
-    },
+    }
 };
 let fs = require('fs');
-fs.readFile('./src/manifest.json','utf8',function(err,files){
+fs.readFile('./src/manifest.json', 'utf8', function (err, files) {
     let result = files.replace(/##version##/g, process.env.npm_package_version);
     fs.writeFile('./public/manifest.json', result, 'utf8', function (err) {
         if (err) return console.log(err);
