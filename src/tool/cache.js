00ce5389b47e8762b7e44353621735eb8869296b
baiy <wo@baiy.org>
2020-02-15 19:56:55 +0800

    二维码生成添加历史记录 #17

--- src/tool/cache.js
+++ src/tool/cache.js
@@ -1,25 +1,28 @@
-import lsCache from "lscache"
-import p from "../../package"
+import lsCache from 'lscache'
+import p from '../../package'
 
 // 缓存key添加版本号
 const cacheNameConvert = function (name) {
-    return "v_" + (p.version.split('.').join("")) + '_' + name
-};
+    return 'v_' + (p.version.split('.').join('')) + '_' + name
+}
 
 export default {
     // expiry 过期时间 秒
-    set(name, value, expiry = 0) {
-        return lsCache.set(cacheNameConvert(name), value, expiry / 60);
+    set (name, value, expiry = 0) {
+        return lsCache.set(cacheNameConvert(name), value, expiry / 60)
     },
-    get(name, defaultValue = null) {
-        let data = lsCache.get(cacheNameConvert(name));
+    get (name, defaultValue = null) {
+        let data = lsCache.get(cacheNameConvert(name))
         return data ? data : defaultValue
     },
-    setnNoVersion(name, value, expiry = 0) {
-        return lsCache.set('nv_' + name, value, expiry / 60);
+    remove (name) {
+        lsCache.remove(cacheNameConvert(name))
     },
-    getNoVersion(name, defaultValue = null) {
-        let data = lsCache.get('nv_' + name);
+    setnNoVersion (name, value, expiry = 0) {
+        return lsCache.set('nv_' + name, value, expiry / 60)
+    },
+    getNoVersion (name, defaultValue = null) {
+        let data = lsCache.get('nv_' + name)
         return data ? data : defaultValue
-    }
+    },
 }
