87db618b027b0e2c99b9149a127809e7208dce93
baiy <wo@baiy.org>
2020-07-09 13:54:59 +0800

    全屏模式下自动调整窗口大小 #19

--- src/tool.js
+++ src/tool.js
@@ -1,20 +1,30 @@
 import Vue from 'vue'
-import ViewUI from 'view-design';
-import 'view-design/dist/styles/iview.css';
+import ViewUI from 'view-design'
+import 'view-design/dist/styles/iview.css'
 import router from './tool.router'
 import optionBlock from './components/optionBlock'
 import codeHighlight from './components/codeHighlight'
-import {plugin as modelPlugin} from './tool/model'
+import { plugin as modelPlugin } from './tool/model'
 import App from './tool.vue'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
-Vue.use(ViewUI);
-Vue.use(modelPlugin);
-Vue.component('option-block',optionBlock);
-Vue.component('code-highlight',codeHighlight);
+Vue.use(ViewUI)
+Vue.use(modelPlugin)
+Vue.component('option-block', optionBlock)
+Vue.component('code-highlight', codeHighlight);
+
+(function () {
+    if (document.body.clientWidth > 900) {
+        console.log("调整窗口大小")
+        const page = document.getElementById('page')
+        page.style.width = 'auto'
+        page.style.padding = '0 50px'
+        page.style.height = 'auto'
+    }
+})()
 
 new Vue({
     router,
-    render: h => h(App)
-}).$mount('#app');
+    render: h => h(App),
+}).$mount('#app')
