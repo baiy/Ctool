1a0a58142e46523a5fc2a3b44332da2f297ad893
baiy <wo@baiy.org>
2021-08-19 15:20:23 +0800

    fix ip

--- src/views/tool/ip.vue
+++ src/views/tool/ip.vue
@@ -20,68 +20,66 @@
     </div>
 </template>
 <script>
-    import axios from "axios"
-    import { codemirror } from 'vue-codemirror'
-    import 'codemirror/lib/codemirror.css'
-    import 'codemirror/mode/javascript/javascript.js'
-    import 'codemirror/addon/fold/foldcode.js'
-    import 'codemirror/addon/fold/foldgutter.js'
-    import 'codemirror/addon/fold/brace-fold.js'
-    import 'codemirror/addon/fold/comment-fold.js'
-    import 'codemirror/addon/fold/foldgutter.css'
+import axios from "axios"
+import {codemirror} from 'vue-codemirror'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/mode/javascript/javascript.js'
+import 'codemirror/addon/fold/foldcode.js'
+import 'codemirror/addon/fold/foldgutter.js'
+import 'codemirror/addon/fold/brace-fold.js'
+import 'codemirror/addon/fold/comment-fold.js'
+import 'codemirror/addon/fold/foldgutter.css'
 
-    export default {
-        components: {
-            codemirror,
-        },
-        created() {
-            this.current = Object.assign(this.current,this.$getToolData("input"))
-        },
-        mounted(){
-            this.codemirror.setSize(null, 350)
-        },
-        computed: {
-            codemirror() {
-                return this.$refs.code.codemirror
+export default {
+    components: {
+        codemirror,
+    },
+    created() {
+        this.current = Object.assign(this.current, this.$getToolData("input"))
+    },
+    mounted() {
+        this.codemirror.setSize(null, 350)
+    },
+    computed: {
+        codemirror() {
+            return this.$refs.code.codemirror
+        }
+    },
+    methods: {
+        handle() {
+            if (this.current.input) {
+                axios({
+                    url: 'https://ifconfig.co/json',
+                    responseType: 'json',
+                    params: this.current.input !== "localhost" ? {ip: this.current.input} : {}
+                }).then(({data}) => {
+                    this.current.output = JSON.stringify(data, null, 4);
+                    this.$saveToolData(this.current);
+                }).catch((error) => {
+                    return this.$Message.error("ip地址信息查询错误:" + error);
+                });
             }
         },
-        methods: {
-            handle() {
-                if (this.current.input) {
-                    axios({
-                        url: 'https://ifconfig.co/json',
-                        responseType: 'json',
-                        params: this.current.input !== "localhost" ? {ip:this.current.input} : {}
-                    }).then((response)=>{
-                        console.log(response)
-                        // if (err) return this.$Message.error("ip地址信息查询错误:"+err);
-                        // this.current.output = JSON.stringify(JSON.parse(result),null, 4);
-                        // this.$saveToolData(this.current);
-                    }).catch((error)=>{
-                        console.log(error)
-                    });
-                }
+        local() {
+            this.current.input = 'localhost';
+            this.handle()
+        }
+    },
+    data() {
+        return {
+            current: {
+                input: "",
+                output: "",
             },
-            local(){
-                this.current.input = 'localhost';
-                this.handle()
-            }
-        },
-        data() {
-            return {
-                current:{
-                    input: "",
-                    output: "",
-                },
-                options: {
-                    mode: 'application/json',
-                    lineNumbers: true,
-                    lineWrapping: false,
-                    foldGutter: true,
-                    indentUnit: 4,
-                    gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter'],
-                },
-            }
-        },
-    }
+            options: {
+                mode: 'application/json',
+                lineNumbers: true,
+                lineWrapping: false,
+                foldGutter: true,
+                indentUnit: 4,
+                gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter'],
+            },
+        }
+    },
+}
 </script>
