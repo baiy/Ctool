62d5ad457291a5fe6f29d595ffd9d3c8bc5e67a7
baiy <wo@baiy.org>
2021-09-08 10:11:06 +0800

    #64 fix

--- src/views/tool/library/ascii.js
+++ src/views/tool/library/ascii.js
@@ -1,5 +1,5 @@
 // ASCII MAP
-import Radix from "radix.js";
+import Radix from "./radix.js";
 
 const ASCII_MAP = ['NUL', 'SOH', 'STX', 'ETX', 'EOT', 'ENQ', 'ACK', 'BEL', 'BS', 'TAB', 'LF', 'VT', 'FF', 'CR', 'SO', 'SI',
     'DLE', 'DC1', 'DC2', 'DC3', 'DC4', 'NAK', 'SYN', 'ETB', 'CAN', 'EM', 'SUB', 'ESC', 'FS', 'GS', 'RS', 'US',
