d19db80b0f05d205b377f9ad2ce632f9a6346a1d
baiy <wo@baiy.org>
2021-09-09 16:00:25 +0800

    简单实现深色主题

--- src/helper.js
+++ src/helper.js
@@ -27,3 +27,9 @@ export const openUrl = (url) => {
 };
 
 export const version = env('version').trim()
+
+export const setDisplayMode = (mode) => {
+    mode = ['light', 'dark', 'auto'].includes(mode) ? mode : 'light'
+    console.log(`set display mode:${mode}`)
+    document.getElementsByTagName('html')[0].setAttribute('theme-mode', mode);
+}
