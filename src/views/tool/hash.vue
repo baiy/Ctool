370c910268dcafbea2a920fcf851eb927426eef4
baiy <wo@baiy.org>
2021-11-13 06:43:32 +0800

    hash 语言包

--- src/views/tool/hash.vue
+++ src/views/tool/hash.vue
@@ -2,9 +2,9 @@
     <div id="tool-hash">
         <Row :gutter="10">
             <Col span="8">
-                <input-block text="内容">
-                    <Input v-model="current.input" :rows="18" type="textarea" placeholder="内容"></Input>
-                    <Checkbox slot="extra" v-model="current.isUppercase">大写字母</Checkbox>
+                <input-block :text="$t('hash_content')">
+                    <Input v-model="current.input" :rows="18" type="textarea" :placeholder="$t('hash_content')"></Input>
+                    <Checkbox slot="extra" v-model="current.isUppercase">{{ $t('hash_uppercase') }}</Checkbox>
                 </input-block>
             </Col>
             <Col span="16">
@@ -25,37 +25,22 @@ export default {
     },
     computed: {
         md5() {
-            if (!this.current.input) {
-                return "";
-            }
             let result = crypto.MD5(this.current.input).toString();
             return this.current.isUppercase ? result.toUpperCase() : result;
         },
         sha1() {
-            if (!this.current.input) {
-                return "";
-            }
             let result = crypto.SHA1(this.current.input).toString();
             return this.current.isUppercase ? result.toUpperCase() : result;
         },
         sha256() {
-            if (!this.current.input) {
-                return "";
-            }
             let result = crypto.SHA256(this.current.input).toString();
             return this.current.isUppercase ? result.toUpperCase() : result;
         },
         sha512() {
-            if (!this.current.input) {
-                return "";
-            }
             let result = crypto.SHA512(this.current.input).toString();
             return this.current.isUppercase ? result.toUpperCase() : result;
         },
         sm3() {
-            if (!this.current.input) {
-                return "";
-            }
             let result = sm.sm3(this.current.input);
             return this.current.isUppercase ? result.toUpperCase() : result;
         },
@@ -63,13 +48,18 @@ export default {
     watch: {
         current: {
             handler() {
-                this.$saveToolData(this.current);
+                if (this.current.input){
+                    this.$saveToolData(this.current);
+                }
             },
             deep: true
         }
     },
     methods:{
         result(type){
+            if (!this.current.input) {
+                return "";
+            }
             return this[type]
         },
         copy(type){
