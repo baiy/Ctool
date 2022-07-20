c243f91b40b0653228f5f1a0152a3598c36ebd4f
baiy <wo@baiy.org>
2022-07-20 18:01:12 +0800

    回滚 `加解密添加对 iv、mode 和 padding 的支持` 修改  功能不是特别完整 后续将对加密模块进行单独分拆处理  提供更加完整的加密实现

--- src/views/tool/encrypt.vue
+++ src/views/tool/encrypt.vue
@@ -2,57 +2,21 @@
     <heightResize ignore :append="['.page-option-block']" @resize="resize">
         <autoHeightTextarea v-model="current.input" :height="inputHeight" :placeholder="$t('encrypt_input')"/>
         <option-block class="page-option-block">
-            <Form :model="formItem" label-position="top">
-            <Form-item label="算法: ">
-                <Select v-model="current.type">
+            <FormItem>
+                <Select v-model="current.type" style="width:200px">
                     <Option v-for="v in type" :value="v" :key="v">{{ v }}</Option>
                 </Select>
-            </Form-item>
-            <Form-item label="密钥: ">
-                <Select v-model="current.keyFormat">
-                    <Option value="Hex">Hex</Option>
-                    <Option value="Base64">Base64</Option>
-                    <Option value="Password">Password</Option>
-                </Select>
-                <Input v-model="current.password" :placeholder="$t('encrypt_password')" type="textarea" :autosize="{minRows: 2, maxRows: 5}"></Input>
-            </Form-item>
-            <Form-item label="IV 向量(Hex 格式): ">
-                <Input v-model="current.iv" :placeholder="$t('iv')" type="textarea" :autosize="{minRows: 3, maxRows: 5}"></Input>
-            </Form-item>
-            <Form-item label="加密模式: " v-if="current.type === 'SM2'">
-                <Select v-model="current.sm2CipherMode">
+            </FormItem>
+            <FormItem>
+                <Input v-model="current.password" :placeholder="$t('encrypt_password')"></Input>
+            </FormItem>
+            <FormItem v-if="current.type === 'SM2'">
+                <Select v-model="current.sm2CipherMode" style="width:100px">
                     <Option value="C1C3C2">C1C3C2</Option>
                     <Option value="C1C2C3">C1C2C3</Option>
                 </Select>
-            </Form-item>
-            <Form-item label="加密模式: " v-else-if="current.type === 'SM4'">
-                <Select v-model="current.mode">
-                    <Option value="ecb">ECB</Option>
-                    <Option value="cbc">CBC</Option>
-                </Select>
-                <Select v-model="current.padding">
-                    <Option value="pkcs#7">Pkcs7</Option>
-                    <Option value="none">NoPadding</Option>
-                </Select>
-            </Form-item>
-            <Form-item label="加密模式: " v-else>
-                <Select v-model="current.mode">
-                    <Option value="ECB">ECB</Option>
-                    <Option value="CBC">CBC</Option>
-                    <Option value="CFB">CFB</Option>
-                    <Option value="OFB">OFB</Option>
-                    <Option value="CTR">CTR</Option>
-                </Select>
-                <Select v-model="current.padding">
-                    <Option value="Pkcs7">Pkcs7</Option>
-                    <Option value="Iso97971">Iso97971</Option>
-                    <Option value="AnsiX923">AnsiX923</Option>
-                    <Option value="Iso10126">Iso10126</Option>
-                    <Option value="ZeroPadding">ZeroPadding</Option>
-                    <Option value="NoPadding">NoPadding</Option>
-                </Select>
-            </Form-item>
-            <Form-item>
+            </FormItem>
+            <FormItem>
                 <ButtonGroup>
                     <Button type="primary" @click="handle('encrypt')">{{ $t('encrypt_encrypt') }}</Button>
                     <Button type="primary" @click="handle('decrypt')">{{ $t('encrypt_decrypt') }}</Button>
@@ -60,8 +24,7 @@
                         {{ $t('encrypt_generate_secret_key') }}
                     </Button>
                 </ButtonGroup>
-            </Form-item>
-            </Form>
+            </FormItem>
         </option-block>
         <autoHeightTextarea :value="current.output" :height="outputHeight" :placeholder="$t('encrypt_output')"/>
     </heightResize>
@@ -70,8 +33,6 @@
 import crypto from "crypto-js"
 import heightResize from "./components/heightResize";
 import autoHeightTextarea from "./components/autoHeightTextarea";
-import { CryptoJS } from 'jsrsasign';
-
 export default {
     components: {
         heightResize,
@@ -94,44 +55,15 @@ export default {
                         case "Rabbit":
                         case "TripleDES":
                             if (v === "encrypt") {
-                                if (this.current.keyFormat !== "Password") {
-                                    output = crypto[this.current.type].encrypt(
-                                        // todo 后续可以考虑支持原文输入的编码格式，以便应对一些二进制格式
-                                        this.current.input,
-                                        // 转换为 WordArray 形式作为密钥传入
-                                        crypto.enc[this.current.keyFormat].parse(this.current.password),
-                                        {
-                                            // IV 要求为 Hex，按照 Hex 解析为 WordArray
-                                            iv: crypto.enc.Hex.parse(this.current.iv),
-                                            mode: crypto.mode[this.current.mode],
-                                            padding: crypto.pad[this.current.padding]
-                                        }
-                                    ).ciphertext;
-                                } else {
-                                    // 兼容 crypto-js 的密码模式
-                                    output = crypto[this.current.type].encrypt(this.current.input, this.current.password).toString();
-                                }
+                                output = crypto[this.current.type].encrypt(
+                                    this.current.input,
+                                    this.current.password
+                                ).toString();
                             } else {
-                                if (this.current.keyFormat !== "Password") {
-                                    // crypto-js 要求解密时传入为 CipherParams，使用 CipherText 构建 CipherParams
-                                    let cipherData = CryptoJS.lib.CipherParams.create({
-                                        ciphertext: crypto.enc.Hex.parse(this.current.input)
-                                    });
-                                    output = crypto[this.current.type].decrypt(
-                                        cipherData, 
-                                        // 转换为 WordArray 形式作为密钥传入
-                                        crypto.enc[this.current.keyFormat].parse(this.current.password),
-                                        {
-                                            // IV 要求为 Hex，按照 Hex 解析为 WordArray
-                                            iv: crypto.enc.Hex.parse(this.current.iv),
-                                            mode: crypto.mode[this.current.mode],
-                                            padding: crypto.pad[this.current.padding]
-                                        }
-                                    ).toString(crypto.enc.Utf8);
-                                } else {
-                                    // 兼容 crypto-js 的密码模式
-                                    output = crypto[this.current.type].decrypt(this.current.input, this.current.password).toString(crypto.enc.Utf8);
-                                }
+                                output = crypto[this.current.type].decrypt(
+                                    this.current.input,
+                                    this.current.password
+                                ).toString(crypto.enc.Utf8);
                             }
                             break;
                         case "SM2":
@@ -141,49 +73,26 @@ export default {
                                     this.current.password,
                                     this.current.sm2CipherMode
                                 );
+                                output = crypto.enc.Base64.stringify(crypto.enc.Hex.parse(output))
                             } else {
+                                let inputHex = crypto.enc.Hex.stringify(crypto.enc.Base64.parse(this.current.input));
                                 output = sm2.doDecrypt(
-                                    this.current.input,
+                                    inputHex,
                                     this.current.password,
                                     this.current.sm2CipherMode
                                 );
+
                             }
                             break;
                         case "SM4":
-                            // sm-crypto 要求传入的 key 为 Hex 格式，因此对于非 Hex 格式的数据进行转换
-                            let sm4Key = this.current.password;
-                            if (this.current.keyFormat === "Base64") {
-                                sm4Key = crypto.enc.Hex.stringify(crypto.enc[this.current.keyFormat].parse(this.current.password));
-                            } else if(this.current.keyFormat === "Password") {
-                                // 如果是 Password，按照 Utf8 编码进行处理，转换为 Hex 格式
-                                // 注意因为库的区别，password 模式跟 crypto-js 的处理不一样
-                                sm4Key = crypto.enc.Hex.stringify(crypto.enc.Utf8.parse(this.current.password));
-                            }
-
                             if(v === "encrypt") {
-                                // SM4 加密
-                                output = sm4.encrypt(
-                                    this.current.input,
-                                    sm4Key, 
-                                    {
-                                        mode: this.current.mode.toLowerCase(), 
-                                        // sm-crypto 传入的 iv 为 Hex 格式
-                                        iv: this.current.iv,
-                                        padding: this.current.padding
-                                    }
-                                );
+                                // SM4 加密，将输出转换为 base64 格式，与 AES 对齐
+                                output = sm4.encrypt(this.current.input, this.current.password);
+                                output = crypto.enc.Base64.stringify(crypto.enc.Hex.parse(output))
                             } else {
-                                // SM4 解密
-                                output = sm4.decrypt(
-                                    this.current.input, 
-                                    sm4Key,
-                                    {
-                                        mode: this.current.mode.toLowerCase(), 
-                                        // sm-crypto 传入的 iv 为 Hex 格式
-                                        iv: this.current.iv,
-                                        padding: this.current.padding
-                                    }
-                                );
+                                // SM4 解密，sm-crypto 要求输入为 hex，将 base64 转换为 hex 作为输入
+                                let inputHex = crypto.enc.Hex.stringify(crypto.enc.Base64.parse(this.current.input));
+                                output = sm4.decrypt(inputHex, this.current.password);
                             }
                     }
                     if (!output) {
@@ -234,10 +143,6 @@ export default {
             current: {
                 input: "",
                 password: "",
-                keyFormat: "Hex",
-                iv: "",
-                mode:"ECB",
-                padding:"Pkcs7",
                 sm2CipherMode: "C1C3C2",
                 output: "",
                 type: "AES",
