98597056d8bbb670685bf9d096cff18fbd5474f6
baiy <baiy@users.noreply.github.com>
2023-01-05 01:54:30 +0000

    deploy: f703c0322d08dbfe5b0a187c4aa5d10b99c84b3a

---
+++ assets/helper-4663133f.js
@@ -0,0 +1,1 @@
+import{bO as n}from"./history-6b8b5fef.js";const e=(o=window.location.href)=>{n.runtime.openUrl(o)},p=(o,a="")=>o.map(r=>({value:r,label:$t(`${a}${r}`)}));export{p as a,e as o};
--- assets/helper-c32b0a15.js
+++
@@ -1,1 +0,0 @@
-import{bO as n}from"./history-d5509b50.js";const e=(o=window.location.href)=>{n.runtime.openUrl(o)},p=(o,a="")=>o.map(r=>({value:r,label:$t(`${a}${r}`)}));export{p as a,e as o};
