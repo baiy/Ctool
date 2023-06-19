508cb133f6450b3acf0960dfeadb8cb8872d5133
baiy <baiy@users.noreply.github.com>
2023-06-19 07:57:50 +0000

    deploy: e0847659660d83e79e9862413b5eb2f61c516e21

---
+++ assets/scss-3ad34a32.js
@@ -0,0 +1,1 @@
+import{p as t}from"./standalone-e10e3a67.js";import{p as r}from"./parser-postcss-6fa00d73.js";import{B as s}from"./base-f0edccdd.js";import"./tool-65d7eee7.js";import"./modulepreload-polyfill-3cfb730f.js";const m=new class extends s{async beautify(){return t.format(this.code,{plugins:[r],parser:"scss",tabWidth:this.getOptionValue("tab",4)})}};export{m as formatter};
