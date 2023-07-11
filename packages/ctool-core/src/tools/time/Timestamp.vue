f0024da61802414ebb1fd4baa0a4c42c5912bed4
baiy <wo@baiy.org>
2023-07-11 17:25:35 +0800

    fix #271 #267

--- packages/ctool-core/src/tools/time/Timestamp.vue
+++ packages/ctool-core/src/tools/time/Timestamp.vue
@@ -7,7 +7,7 @@
             :label="$t('time_timezone')"
             v-model="action.current.timezone"
             :options="timezoneLists"
-            @change="(value)=>action.current.timezone = value"
+            @change="value => (action.current.timezone = value)"
         />
         <Display position="right-center">
             <Input
@@ -23,14 +23,14 @@
                         v-model="action.current.format"
                         size="small"
                         :options="[
-                        {
-                            value:`auto`,
-                            label:`${$t('time_unix_auto')}:${$t(`time_unix_${output.autoFormat}`)}`
-                        },
-                        {value:Format.second,label:$t('time_unix_second')},
-                        {value:Format.millisecond,label:$t('time_unix_millisecond')},
-                        {value:Format.nanosecond,label:$t('time_unix_nanosecond')},
-                    ]"
+                            {
+                                value: `auto`,
+                                label: `${$t('time_unix_auto')}:${$t(`time_unix_${output.autoFormat}`)}`,
+                            },
+                            { value: Format.second, label: $t('time_unix_second') },
+                            { value: Format.millisecond, label: $t('time_unix_millisecond') },
+                            { value: Format.nanosecond, label: $t('time_unix_nanosecond') },
+                        ]"
                     />
                     <Button
                         v-if="action.current.input !== ''"
@@ -41,35 +41,51 @@
                 </Align>
             </template>
         </Display>
-        <Display position="right-center" :text="output.isValid ? $t(`main_ui_copy`) : ''" @click="()=>$copy(output.second)">
-            <Input readonly size="large" :model-value="output.second" :label="$t(`time_second`)"/>
+        <Display
+            position="right-center"
+            :text="output.isValid ? $t(`main_ui_copy`) : ''"
+            @click="() => $copy(output.second)"
+        >
+            <Input readonly size="large" :model-value="output.second" :label="$t(`time_second`)" />
         </Display>
         <div v-row="`1-1`">
-            <Display position="right-center" :text="output.isValid ? $t(`main_ui_copy`) : ''" @click="()=>$copy(output.millisecond)">
-                <Input readonly size="large" :model-value="output.millisecond" :label="$t(`time_millisecond`)"/>
+            <Display
+                position="right-center"
+                :text="output.isValid ? $t(`main_ui_copy`) : ''"
+                @click="() => $copy(output.millisecond)"
+            >
+                <Input readonly size="large" :model-value="output.millisecond" :label="$t(`time_millisecond`)" />
             </Display>
-            <Display position="right-center" :text="output.isValid ? $t(`main_ui_copy`) : ''" @click="()=>$copy(output.nanosecond)">
-                <Input readonly size="large" :model-value="output.nanosecond" :label="$t(`time_nanosecond`)"/>
+            <Display
+                position="right-center"
+                :text="output.isValid ? $t(`main_ui_copy`) : ''"
+                @click="() => $copy(output.nanosecond)"
+            >
+                <Input readonly size="large" :model-value="output.nanosecond" :label="$t(`time_nanosecond`)" />
             </Display>
         </div>
         <div>
             <Table
                 :columns="[
-                        {title:$t(`time_format`),key:`format`},
-                        {title:$t(`time_value`),key:`value`},
-                    ]"
+                    { title: $t(`time_format`), key: `format` },
+                    { title: $t(`time_value`), key: `value` },
+                ]"
                 :lists="example"
                 :action-width="100"
                 border
             >
-                <template #column="{row}">
+                <template #column="{ row }">
                     <td>{{ row.format }}</td>
                     <td>
                         <Link @click="$copy(row.value)">{{ row.value }}</Link>
                     </td>
                 </template>
-                <template #default="{row}">
-                    <Button :text="$t(`main_ui_load`)" @click="()=>action.current.input = `${row.value}`" :size="'small'"/>
+                <template #default="{ row }">
+                    <Button
+                        :text="$t(`main_ui_load`)"
+                        @click="() => (action.current.input = `${row.value}`)"
+                        :size="'small'"
+                    />
                 </template>
             </Table>
         </div>
@@ -77,68 +93,80 @@
 </template>
 
 <script lang="ts" setup>
-import {useAction, initialize} from "@/store/action";
-import dayjs from "dayjs"
-import utc from "dayjs/plugin/utc"
-import timezone from "dayjs/plugin/timezone"
-import {onUnmounted, watch} from "vue"
+import { useAction, initialize } from "@/store/action";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+import { onUnmounted, watch } from "vue";
 import zhTimezone from "./timezone/zh_CN.json";
 import enTimezone from "./timezone/en_US.json";
-import {SelectOption} from "@/types";
-import {Format, transform, InputType} from "./util/timestamp";
+import { SelectOption } from "@/types";
+import { Format, transform, InputType } from "./util/timestamp";
 
-dayjs.extend(utc)
-dayjs.extend(timezone)
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
-const format = "auto" as (Format | "auto")
+const format = "auto" as Format | "auto";
 
-const action = useAction(await initialize({
-        input: "",
-        timezone: dayjs.tz.guess(),
-        format
-    },
-    {
-        paste: (str) => (new RegExp(/^\d+-\d+-\d+ \d+:\d+:\d+$/)).test(str) || (new RegExp(/^\d+-\d+-\d+ \d+:\d+:\d+\.\d+$/)).test(str) || (new RegExp(/^-?\d{5,}$/)).test(str)
-    }
-))
+const action = useAction(
+    await initialize(
+        {
+            input: "",
+            timezone: dayjs.tz.guess(),
+            format,
+        },
+        {
+            paste: str =>
+                new RegExp(/^\d+-\d+-\d+ \d+:\d+:\d+$/).test(str) ||
+                new RegExp(/^\d+-\d+-\d+ \d+:\d+:\d+\.\d+$/).test(str) ||
+                new RegExp(/^-?\d{5,}$/).test(str),
+        },
+    ),
+);
 
 const output = $computed(() => {
-    return transform((action.current.input || "").trim(), action.current.timezone, action.current.format)
-})
+    return transform((action.current.input || "").trim(), action.current.timezone, action.current.format);
+});
 
-watch(() => {
-    return {
-        data: action.current,
-        is_valid: output.isValid,
-        format: output.format
-    }
-}, ({is_valid}) => {
-    if (is_valid) {
-        action.save()
-    }
-}, {immediate: true, deep: true})
+watch(
+    () => {
+        return {
+            data: action.current,
+            is_valid: output.isValid,
+            format: output.format,
+        };
+    },
+    ({ is_valid }) => {
+        if (is_valid) {
+            action.save();
+        }
+    },
+    { immediate: true, deep: true },
+);
 
 const timezoneLists: SelectOption = $computed(() => {
-    return Object.entries($t('main_locale') === "zh_CN" ? zhTimezone : enTimezone).map(([key, value]) => {
-        return {value: key, label: `${value}`}
-    })
-})
+    return (
+        Object.entries($t("main_locale") === "zh_CN" ? zhTimezone : enTimezone).map(([key, value]) => {
+            return { value: key, label: `${value}` };
+        }) || []
+    );
+});
 
-let current = $ref(dayjs().valueOf())
+let current = $ref(dayjs().valueOf());
 const currentTimer = setInterval(() => {
-    current = dayjs().valueOf()
-}, 100)
+    current = dayjs().valueOf();
+}, 100);
 onUnmounted(() => {
     clearInterval(currentTimer);
-})
+});
 
 const example = $computed(() => {
-    const day = dayjs(current).tz(action.current.timezone)
+    const day = dayjs(current).tz(action.current.timezone);
     return [
-        {format: $t('time_normal_second'), value: day.format('YYYY-MM-DD HH:mm:ss')},
-        {format: $t('time_unix_second'), value: day.unix().toString()},
-        {format: $t('time_normal_millisecond'), value: day.format('YYYY-MM-DD HH:mm:ss.SSS')},
-        {format: $t('time_unix_millisecond'), value: day.valueOf().toString()},
-    ]
-})
+        { format: $t("time_normal_second"), value: day.format("YYYY-MM-DD HH:mm:ss") },
+        { format: $t("time_unix_second"), value: day.unix().toString() },
+        { format: $t("time_normal_millisecond"), value: day.format("YYYY-MM-DD HH:mm:ss.SSS") },
+        { format: $t("time_unix_millisecond"), value: day.valueOf().toString() },
+    ];
+});
 </script>
--- packages/ctool-core/src/tools/time/Timezone.vue
+++ packages/ctool-core/src/tools/time/Timezone.vue
@@ -1,7 +1,11 @@
 <template>
     <Align direction="vertical">
-        <Display position="right-center" v-for="i of range(0,7)">
-            <Input v-model="outputInstance[`output${i}`].value" :placeholder="$t('time_timezone_input_placeholder')" :size="size"/>
+        <Display position="right-center" v-for="i of range(0, 7)">
+            <Input
+                v-model="outputInstance[`output${i}`].value"
+                :placeholder="$t('time_timezone_input_placeholder')"
+                :size="size"
+            />
             <template #extra>
                 <Align>
                     <Select
@@ -10,14 +14,20 @@
                         :size="'small'"
                         :disabled="isValid && action.current.timezone[i] === action.current.type"
                     />
-                    <Button v-if="isValid" :text="$t(`main_ui_copy`)" @click="$copy(outputInstance[`output${i}`].value)" :size="'small'" :type="'primary'"/>
+                    <Button
+                        v-if="isValid"
+                        :text="$t(`main_ui_copy`)"
+                        @click="$copy(outputInstance[`output${i}`].value)"
+                        :size="'small'"
+                        :type="'primary'"
+                    />
                 </Align>
             </template>
         </Display>
-        <Button :size="size" v-if="!isValid" :text="$t(`time_current_time`)" @click="setCurrent"/>
+        <Button :size="size" v-if="!isValid" :text="$t(`time_current_time`)" @click="setCurrent" />
         <div v-row="`1-100px`" v-else>
-            <Button :size="size" @click="isMore = true" :text="$t(`main_ui_more`)"/>
-            <Button :size="size" @click="action.current.input = ``" :text="$t(`main_ui_clear`)"/>
+            <Button :size="size" @click="isMore = true" :text="$t(`main_ui_more`)" />
+            <Button :size="size" @click="action.current.input = ``" :text="$t(`main_ui_clear`)" />
         </div>
     </Align>
     <ExtendPage v-model="isMore">
@@ -27,27 +37,27 @@
                 position="right-center"
                 :text="item['label']"
                 :type="item['value'] === action.current.type ? `danger` : `general`"
-                @click="()=>$copy(convert(action.current.input,action.current.type,item['value']))"
+                @click="() => $copy(convert(action.current.input, action.current.type, item['value']))"
             >
-                <Input :model-value="convert(action.current.input,action.current.type,item['value'])" :size="size"/>
+                <Input :model-value="convert(action.current.input, action.current.type, item['value'])" :size="size" />
             </Display>
         </Align>
     </ExtendPage>
 </template>
 
 <script lang="ts" setup>
-import {initialize, useAction} from "@/store/action";
-import {range} from "lodash";
-import {watch, computed} from "vue";
-import {SelectOption, ComponentSizeType} from "@/types";
-import dayjs from "dayjs"
-import utc from "dayjs/plugin/utc"
-import timezone from "dayjs/plugin/timezone"
+import { initialize, useAction } from "@/store/action";
+import { range } from "lodash";
+import { watch, computed } from "vue";
+import { SelectOption, ComponentSizeType } from "@/types";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
 import zhTimezone from "./timezone/zh_CN.json";
 import enTimezone from "./timezone/en_US.json";
 
-dayjs.extend(utc)
-dayjs.extend(timezone)
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 const size: ComponentSizeType = "large";
 const defaultTimezoneLists = [
@@ -57,135 +67,147 @@ const defaultTimezoneLists = [
     "America/Chicago",
     "Europe/Berlin",
     "Africa/Cairo",
-    "Asia/Calcutta"
-]
+    "Asia/Calcutta",
+];
 const timezoneLists: SelectOption = $computed(() => {
-    return Object.entries($t('main_locale') === "zh_CN" ? zhTimezone : enTimezone).map(([key, value]) => {
-        return {value: key, label: `${value}`}
-    })
-})
+    return (
+        Object.entries($t("main_locale") === "zh_CN" ? zhTimezone : enTimezone).map(([key, value]) => {
+            return { value: key, label: `${value}` };
+        }) || []
+    );
+});
 
-const check = (value) => {
-    return (new RegExp(/^\d+-\d+-\d+ \d+:\d+:\d+$/)).test(value.trim())
-        || (new RegExp(/^\d+-\d+-\d+ \d+:\d+:\d+\.\d+$/)).test(value.trim())
-}
+const check = (value: string) => {
+    return (
+        new RegExp(/^\d+-\d+-\d+ \d+:\d+:\d+$/).test(value.trim()) ||
+        new RegExp(/^\d+-\d+-\d+ \d+:\d+:\d+\.\d+$/).test(value.trim())
+    );
+};
 
-const action = useAction(await initialize({
-    type: "",
-    input: "",
-    timezone: [...(new Set([dayjs.tz.guess(), ...defaultTimezoneLists]))].slice(0, 7),
-}, {paste: false}))
-let isMore = $ref(false)
+const action = useAction(
+    await initialize(
+        {
+            type: "",
+            input: "",
+            timezone: [...new Set([dayjs.tz.guess(), ...defaultTimezoneLists])].slice(0, 7),
+        },
+        { paste: false },
+    ),
+);
+let isMore = $ref(false);
 
 const getHandle = (target: string) => {
     if (!action.current.type || action.current.input.trim() === "") {
-        return ""
+        return "";
     }
     if (action.current.type === target) {
-        return action.current.input
+        return action.current.input;
     }
     try {
         if (!isValid) {
-            throw new Error($t('time_error_format'))
+            throw new Error($t("time_error_format"));
         }
-        return convert(action.current.input, action.current.type, target)
+        return convert(action.current.input, action.current.type, target);
     } catch (e) {
-        return $error(e)
+        return $error(e);
     }
-}
+};
 
 const setHandle = (source: string, value: string) => {
-    action.current.input = value
-    action.current.type = source
-}
+    action.current.input = value;
+    action.current.type = source;
+};
 
-watch(() => action.current, (current) => {
-    if (!check(current.input) || current.type === "") {
-        return;
-    }
-    action.save()
-}, {immediate: true, deep: true})
-
+watch(
+    () => action.current,
+    current => {
+        if (!check(current.input) || current.type === "") {
+            return;
+        }
+        action.save();
+    },
+    { immediate: true, deep: true },
+);
 
 const isValid = $computed(() => {
-    return check(action.current.input.trim())
-})
+    return check(action.current.input.trim());
+});
 
 const convert = (input: string, source: string, target: string) => {
-    const isMillisecond = input.includes('.')
+    const isMillisecond = input.includes(".");
     if (isMillisecond) {
-        return dayjs.tz(dayjs.tz(input, source), target).format('YYYY-MM-DD HH:mm:ss.SSS')
+        return dayjs.tz(dayjs.tz(input, source), target).format("YYYY-MM-DD HH:mm:ss.SSS");
     }
-    return dayjs.tz(dayjs.tz(input, source), target).format('YYYY-MM-DD HH:mm:ss')
-}
+    return dayjs.tz(dayjs.tz(input, source), target).format("YYYY-MM-DD HH:mm:ss");
+};
 
 const setCurrent = () => {
-    action.current.type = action.current.timezone[0]
-    action.current.input = dayjs.tz(dayjs(), action.current.type).format('YYYY-MM-DD HH:mm:ss')
-}
+    action.current.type = action.current.timezone[0];
+    action.current.input = dayjs.tz(dayjs(), action.current.type).format("YYYY-MM-DD HH:mm:ss");
+};
 
 const output0 = computed({
     get: () => {
-        return getHandle(action.current.timezone[0])
+        return getHandle(action.current.timezone[0]);
     },
-    set: (value) => {
-        return setHandle(action.current.timezone[0], value)
-    }
-})
+    set: value => {
+        return setHandle(action.current.timezone[0], value);
+    },
+});
 
 const output1 = computed({
     get: () => {
-        return getHandle(action.current.timezone[1])
+        return getHandle(action.current.timezone[1]);
     },
-    set: (value) => {
-        return setHandle(action.current.timezone[1], value)
-    }
-})
+    set: value => {
+        return setHandle(action.current.timezone[1], value);
+    },
+});
 
 const output2 = computed({
     get: () => {
-        return getHandle(action.current.timezone[2])
+        return getHandle(action.current.timezone[2]);
     },
-    set: (value) => {
-        return setHandle(action.current.timezone[2], value)
-    }
-})
+    set: value => {
+        return setHandle(action.current.timezone[2], value);
+    },
+});
 
 const output3 = computed({
     get: () => {
-        return getHandle(action.current.timezone[3])
+        return getHandle(action.current.timezone[3]);
     },
-    set: (value) => {
-        return setHandle(action.current.timezone[3], value)
-    }
-})
+    set: value => {
+        return setHandle(action.current.timezone[3], value);
+    },
+});
 
 const output4 = computed({
     get: () => {
-        return getHandle(action.current.timezone[4])
+        return getHandle(action.current.timezone[4]);
     },
-    set: (value) => {
-        return setHandle(action.current.timezone[4], value)
-    }
-})
+    set: value => {
+        return setHandle(action.current.timezone[4], value);
+    },
+});
 
 const output5 = computed({
     get: () => {
-        return getHandle(action.current.timezone[5])
+        return getHandle(action.current.timezone[5]);
     },
-    set: (value) => {
-        return setHandle(action.current.timezone[5], value)
-    }
-})
+    set: value => {
+        return setHandle(action.current.timezone[5], value);
+    },
+});
 
 const output6 = computed({
     get: () => {
-        return getHandle(action.current.timezone[6])
+        return getHandle(action.current.timezone[6]);
     },
-    set: (value) => {
-        return setHandle(action.current.timezone[6], value)
-    }
-})
+    set: value => {
+        return setHandle(action.current.timezone[6], value);
+    },
+});
 
 const outputInstance = {
     output0,
@@ -195,5 +217,5 @@ const outputInstance = {
     output4,
     output5,
     output6,
-}
+};
 </script>
