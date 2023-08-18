660959c6d82e3e68cce7e6c4c8c5f0dde3ebe0a0
baiy <wo@baiy.org>
2023-08-18 14:38:48 +0800

    #278 fix

--- packages/ctool-core/src/components/text/TextOutput.vue
+++ packages/ctool-core/src/components/text/TextOutput.vue
@@ -1,28 +1,30 @@
 <template>
     <Display position="bottom-right" :style="style" toggle>
-        <Textarea :model-value="error" v-if="error !== ''" readonly/>
-        <Display position="center" v-else-if="result === ''" style="height: 100%;">
-            <Textarea readonly/>
+        <Textarea :model-value="error" v-if="error !== ''" readonly />
+        <Display position="center" v-else-if="result === ''" style="height: 100%">
+            <Textarea readonly />
             <template #extra>
-                <Exception/>
+                <Exception />
             </template>
         </Display>
         <template v-else>
-            <Textarea :model-value="result" readonly :placeholder="placeholder" v-if="['text', 'base64', 'hex', 'hex_dump'].includes(current.type)"/>
+            <Textarea
+                :model-value="result"
+                readonly
+                :placeholder="placeholder"
+                v-if="['text', 'base64', 'hex', 'hex_dump'].includes(current.type)"
+            />
             <Card v-else height="100%">
                 <Align direction="vertical" horizontal="center" vertical="center">
                     <img
                         v-if="current.type === 'image'"
                         @click="copyImage(result)"
                         :src="result"
-                        style="cursor:pointer;border: 1px dashed #666;max-width: 80%;max-height: 80%;"
+                        style="cursor: pointer; border: 1px dashed #666; max-width: 80%; max-height: 80%"
                     />
-                    <template v-if="['image','down'].includes(current.type)">
-                        <span style="font-size: .75rem;color: var(--ctool-placeholder-text-color);">
-                            {{ content.name() }}
-                            <template v-if="content.isImage()">
-                                {{content.imageSizeString ? `(${content.imageSizeString})` : ''}}
-                            </template>
+                    <template v-if="['image', 'down'].includes(current.type)">
+                        <span style="font-size: 0.75rem; color: var(--ctool-placeholder-text-color)">
+                            {{ content.name() }} {{ content.imageSizeString ? `(${content.imageSizeString})` : "" }}
                         </span>
                         <Button size="small" type="primary" @click="down">{{ $t(`main_ui_down`) }}</Button>
                     </template>
@@ -33,20 +35,30 @@
             <Align>
                 <template v-if="result !== ''">
                     <template v-if="current.type === 'base64'">
-                        <Bool size="small" v-model="current.option.base64.is_url_safe" border :label="$t(`component_content_output_url_safe`)"
-                              :disabled="current.option.base64.data_url_show"/>
-                        <Bool size="small" v-model="current.option.base64.data_url_show" border :label="$t(`component_content_output_data_url`)"/>
+                        <Bool
+                            size="small"
+                            v-model="current.option.base64.is_url_safe"
+                            border
+                            :label="$t(`component_content_output_url_safe`)"
+                            :disabled="current.option.base64.data_url_show"
+                        />
+                        <Bool
+                            size="small"
+                            v-model="current.option.base64.data_url_show"
+                            border
+                            :label="$t(`component_content_output_data_url`)"
+                        />
                     </template>
                     <template v-if="current.type === 'text'">
                         <Select
                             v-if="encoding && current.type === 'text'"
                             size="small"
                             :options="[
-                            {
-                                label:`${$t('component_content_output_analyse_encoding')}: ${analyseEncoding}`,
-                                value:`analyse`
-                            },
-                            ...encodings
+                                {
+                                    label: `${$t('component_content_output_analyse_encoding')}: ${analyseEncoding}`,
+                                    value: `analyse`,
+                                },
+                                ...encodings,
                             ]"
                             v-model="current.option.text.encoding"
                         />
@@ -56,7 +68,8 @@
                             <Select
                                 :label="$t(`component_content_output_hex_dump_format`)"
                                 size="small"
-                                v-model="current.option.hex.format" :options="hexFormat"
+                                v-model="current.option.hex.format"
+                                :options="hexFormat"
                             />
                             <InputNumber
                                 size="small"
@@ -70,12 +83,14 @@
                         <Select
                             :label="$t(`component_content_output_hex_caps`)"
                             size="small"
-                            v-model="current.option.hex.caps" :options="hexCaps"
+                            v-model="current.option.hex.caps"
+                            :options="hexCaps"
                         />
                         <Select
                             :label="$t(`component_content_output_hex_type`)"
                             size="small"
-                            v-model="current.option.hex.type" :options="hexType"
+                            v-model="current.option.hex.type"
+                            :options="hexType"
                         />
                     </template>
                 </template>
@@ -83,7 +98,11 @@
                     size="small"
                     v-model="current.type"
                     v-if="typeLists.length > 1"
-                    :options="typeLists.map((item)=>{return {value:item,label:$t(`component_content_type_${item}`)}})"
+                    :options="
+                        typeLists.map(item => {
+                            return { value: item, label: $t(`component_content_type_${item}`) };
+                        })
+                    "
                 />
             </Align>
         </template>
@@ -91,167 +110,166 @@
 </template>
 
 <script setup lang="ts">
-import {TextOutput, createTextOutput} from "./index"
-import {copyImage as _copyImage} from "@/helper/clipboard"
-import {nextTick, PropType, StyleValue, watch} from "vue"
-import Text, {encodings, Encoding} from "@/helper/text"
-import {textOutputEncoderLists, TextOutputEncoderType} from "@/types"
-import {sizeConvert} from "@/components/util";
+import { TextOutput, createTextOutput } from "./index";
+import { copyImage as _copyImage } from "@/helper/clipboard";
+import { nextTick, PropType, StyleValue, watch } from "vue";
+import Text, { encodings, Encoding } from "@/helper/text";
+import { textOutputEncoderLists, TextOutputEncoderType } from "@/types";
+import { sizeConvert } from "@/components/util";
 import Message from "@/helper/message";
 
 const props = defineProps({
     modelValue: {
         type: Object as PropType<TextOutput>,
-        default: () => createTextOutput()
+        default: () => createTextOutput(),
     },
     height: {
         type: [String, Number],
-        default: ""
+        default: "",
     },
     placeholder: {
         type: String,
         default: () => {
-            return $t("main_ui_output")
-        }
+            return $t("main_ui_output");
+        },
     },
     allow: {
         type: Array as PropType<TextOutputEncoderType[]>,
-        default: () => textOutputEncoderLists
+        default: () => textOutputEncoderLists,
     },
     content: {
         type: Object as PropType<Text>,
-        default: () => Text.empty()
+        default: () => Text.empty(),
     },
     encoding: {
         type: Boolean,
-        default: false
-    }
+        default: false,
+    },
+});
+const emit = defineEmits<{ (e: "update:modelValue", modelValue: TextOutput): void; (e: "success"): void }>();
 
-})
-const emit = defineEmits<{ (e: 'update:modelValue', modelValue: TextOutput): void, (e: 'success'): void }>()
-
-
-let exception = $ref("")
+let exception = $ref("");
 
 const current: TextOutput = $computed({
     get: () => props.modelValue,
-    set: (value) => emit('update:modelValue', value)
-})
-
+    set: value => emit("update:modelValue", value),
+});
 
 const typeLists = $computed(() => {
-    return textOutputEncoderLists.filter((item) => props.allow.includes(item));
-})
+    return textOutputEncoderLists.filter(item => props.allow.includes(item));
+});
 
-const success = () => emit("success")
+const success = () => emit("success");
 const down = () => {
-    exception = ""
+    exception = "";
     try {
-        const content: Text = props.content
-        content.toDown()
-        success()
+        const content: Text = props.content;
+        content.toDown();
+        success();
     } catch (e) {
-        exception = $error(e)
+        exception = $error(e);
     }
-}
+};
 
 const error = $computed(() => {
     if (props.content.isEmpty()) {
-        return ""
+        return "";
     }
     if (props.content.isError()) {
-        return `${props.content.toString()}`
+        return `${props.content.toString()}`;
     }
     if (exception !== "") {
-        return `${exception}`
+        return `${exception}`;
     }
-    return ""
-})
+    return "";
+});
 
-let result = $ref("")
+let result = $ref("");
 
-let analyseEncoding = $ref<Encoding>("utf-8")
+let analyseEncoding = $ref<Encoding>("utf-8");
 
 const copyImage = (base64: string) => {
+    console.log(base64);
     _copyImage(base64, () => {
-        Message.success($t('main_ui_copy_image_ok'))
-    })
-}
+        Message.success($t("main_ui_copy_image_ok"));
+    });
+};
 
-watch(() => {
-    return {
-        data: props.content,
-        type: current.type,
-        option: current.option,
-        isEncoding: props.encoding,
-    }
-}, async ({data, type, option, isEncoding}) => {
-    await nextTick()
-    exception = ""
-    if (data.isError() || data.isEmpty()) {
-        result = ""
-        return
-    }
-    try {
-        let r: string = "";
-        switch (type) {
-            case "text":
-                analyseEncoding = data.analyseEncoding()
-                if (!isEncoding) {
-                    r = data.toString()
-                } else {
-                    r = data.toString(option.text.encoding === "analyse" ? analyseEncoding : option.text.encoding)
-                }
-                break;
-            case "base64":
-                r = data.toBase64(
-                    option.base64.is_url_safe,
-                    option.base64.data_url_show
-                )
-                break;
-            case "hex":
-                r = data.toHex(option.hex)
-                break;
-            case "image":
-                if (!data.isImage()) {
-                    throw new Error("Not Image File")
-                }
-                await data.calculateImageSize()
-                r = data.toDataUrl()
-                break;
+watch(
+    () => {
+        return {
+            data: props.content,
+            type: current.type,
+            option: current.option,
+            isEncoding: props.encoding,
+        };
+    },
+    async ({ data, type, option, isEncoding }) => {
+        await nextTick();
+        exception = "";
+        if (data.isError() || data.isEmpty()) {
+            result = "";
+            return;
         }
-        if (r !== "") {
-            success()
-            result = r
+        try {
+            let r: string = "";
+            switch (type) {
+                case "text":
+                    analyseEncoding = data.analyseEncoding();
+                    if (!isEncoding) {
+                        r = data.toString();
+                    } else {
+                        r = data.toString(option.text.encoding === "analyse" ? analyseEncoding : option.text.encoding);
+                    }
+                    break;
+                case "base64":
+                    r = data.toBase64(option.base64.is_url_safe, option.base64.data_url_show);
+                    break;
+                case "hex":
+                    r = data.toHex(option.hex);
+                    break;
+                case "image":
+                    if (!data.isImage()) {
+                        throw new Error("Not Image File");
+                    }
+                    await data.calculateImageSize();
+                    r = data.toDataUrl();
+                    break;
+            }
+            if (r !== "") {
+                success();
+                result = r;
+            }
+        } catch (e) {
+            exception = $error(e);
         }
-    } catch (e) {
-        exception = $error(e)
-    }
-}, {immediate: true, deep: true})
+    },
+    { immediate: true, deep: true },
+);
 
 const style = $computed(() => {
-    let css: StyleValue = {}
+    let css: StyleValue = {};
     if (props.height) {
-        css.height = sizeConvert(props.height)
+        css.height = sizeConvert(props.height);
     }
-    return css
-})
+    return css;
+});
 
 const hexType = [
-    {value: 'hex', label: $t(`component_content_output_hex_hex`)},
-    {value: 'dump', label: $t(`component_content_output_hex_dump`)}
-]
+    { value: "hex", label: $t(`component_content_output_hex_hex`) },
+    { value: "dump", label: $t(`component_content_output_hex_dump`) },
+];
 
 const hexCaps = [
-    {value: 'lower', label: $t(`component_content_output_hex_lower`)},
-    {value: 'upper', label: $t(`component_content_output_hex_upper`)}
-]
+    { value: "lower", label: $t(`component_content_output_hex_lower`) },
+    { value: "upper", label: $t(`component_content_output_hex_upper`) },
+];
 
 const hexFormat = [
-    {value: 'twos', label: $t(`component_content_output_hex_dump_format_twos`)},
-    {value: 'fours', label: $t(`component_content_output_hex_dump_format_fours`)},
-    {value: 'eights', label: $t(`component_content_output_hex_dump_format_eights`)},
-    {value: 'sixteens', label: $t(`component_content_output_hex_dump_format_sixteens`)},
-    {value: 'none', label: $t(`component_content_output_hex_dump_format_none`)}
-]
+    { value: "twos", label: $t(`component_content_output_hex_dump_format_twos`) },
+    { value: "fours", label: $t(`component_content_output_hex_dump_format_fours`) },
+    { value: "eights", label: $t(`component_content_output_hex_dump_format_eights`) },
+    { value: "sixteens", label: $t(`component_content_output_hex_dump_format_sixteens`) },
+    { value: "none", label: $t(`component_content_output_hex_dump_format_none`) },
+];
 </script>
--- packages/ctool-core/src/helper/text/index.ts
+++ packages/ctool-core/src/helper/text/index.ts
@@ -1,30 +1,31 @@
-import filetype from 'magic-bytes.js'
-import {isText, isBinary} from 'istextorbinary'
-import dayjs from 'dayjs'
-import {FormatOptions as hexyFormatOptions, hexy} from "hexy";
-import {Base64} from 'js-base64'
-import validDataUrl from 'valid-data-url'
-import {Buffer} from 'buffer'
-import Uint from './uint';
-import extList from 'ext-list';
+import filetype from "magic-bytes.js";
+import { isText, isBinary } from "istextorbinary";
+import dayjs from "dayjs";
+import { FormatOptions as hexyFormatOptions, hexy } from "hexy";
+import { Base64 } from "js-base64";
+import validDataUrl from "valid-data-url";
+import { Buffer } from "buffer";
+import Uint from "./uint";
+import extList from "ext-list";
 
-const _encoding = ["gbk", "utf-8"] as const
-export type Encoding = typeof _encoding[number]
-export const encodings: string[] = _encoding as any
+const _encoding = ["gbk", "utf-8"] as const;
+export type Encoding = (typeof _encoding)[number];
+export const encodings: string[] = _encoding as any;
 
-const extMimeList: Map<string, string> = extList()
+const extMimeList: Map<string, string> = extList();
 
 // 通用数据类型
 class Text {
-    private uint: Uint
-    private fileName: string = ""
-    private width = 0
-    private height = 0
+    private uint: Uint;
+    private fileName: string = "";
+    private width?: number;
+    private height?: number;
+    public imageSizeString: string = "";
     // 是否为错误内容类型
-    private _isError: boolean = false
+    private _isError: boolean = false;
 
     constructor(uint: Uint) {
-        this.uint = uint
+        this.uint = uint;
     }
 
     static empty(): Text {
@@ -32,129 +33,130 @@ class Text {
     }
 
     static fromError(error: string) {
-        const instance = Text.fromString(error)
-        instance._isError = true
-        return instance
+        const instance = Text.fromString(error);
+        instance._isError = true;
+        return instance;
     }
 
     static async fromBlob(item: Blob) {
-        return new Promise<Text>((resolve) => {
-            const reader = new FileReader()
-            reader.readAsArrayBuffer(item)
+        return new Promise<Text>(resolve => {
+            const reader = new FileReader();
+            reader.readAsArrayBuffer(item);
             reader.onloadend = () => {
-                resolve(new Text(new Uint(new Uint8Array(reader.result as ArrayBuffer))))
-            }
-        })
+                resolve(new Text(new Uint(new Uint8Array(reader.result as ArrayBuffer))));
+            };
+        });
     }
 
     static async fromUrl(url: string) {
-        return new Promise<Text>((resolve) => {
+        return new Promise<Text>(resolve => {
             const xhr = new XMLHttpRequest();
-            xhr.open('GET', url);
-            xhr.responseType = 'blob';
+            xhr.open("GET", url);
+            xhr.responseType = "blob";
             xhr.onload = async () => {
                 if (xhr.status >= 200 && xhr.status < 300) {
-                    resolve(await Text.fromBlob(xhr.response))
+                    resolve(await Text.fromBlob(xhr.response));
                 } else {
                     resolve(Text.fromError(`[${url}] load fail, status code:${xhr.status}`));
                 }
             };
-            xhr.onerror = (e) => {
-                resolve(Text.fromError(`[${url}] load fail`))
+            xhr.onerror = e => {
+                resolve(Text.fromError(`[${url}] load fail`));
             };
             xhr.send();
-        })
+        });
     }
 
     static fromUint8Array(item: Uint8Array, encoding: Encoding = "utf-8") {
-        return new Text(new Uint(item, encoding))
+        return new Text(new Uint(item, encoding));
     }
 
     static fromString(str: string, toEncoding: Encoding = "utf-8"): Text {
-        return new Text(Uint.fromString(str).to(toEncoding))
+        return new Text(Uint.fromString(str).to(toEncoding));
     }
 
-    static fromHex(
-        str: string,
-        {preserve_line_breaks = false}: { preserve_line_breaks?: boolean } = {}
-    ): Text {
+    static fromHex(str: string, { preserve_line_breaks = false }: { preserve_line_breaks?: boolean } = {}): Text {
         return Text.fromBuffer(
             Buffer.from(
-                str.replaceAll(" ", "") // 过滤空格
+                str
+                    .replaceAll(" ", "") // 过滤空格
                     .replaceAll("\n", preserve_line_breaks ? "0a" : "") // 换行符处理
                     .trim(),
-                'hex'
-            )
-        )
+                "hex",
+            ),
+        );
     }
 
     static fromBuffer(item: Buffer): Text {
-        return Text.fromUint8Array(Uint8Array.from(item))
+        return Text.fromUint8Array(Uint8Array.from(item));
     }
 
     static fromArray(item: number[]): Text {
-        return Text.fromUint8Array(Uint8Array.from(item))
+        return Text.fromUint8Array(Uint8Array.from(item));
     }
 
     static fromBase64(str: string): Text {
-        let item = str.trim()
+        let item = str.trim();
         // 兼容DataUrl
         if (validDataUrl(item)) {
-            item = item.split(',')?.pop() || ""
+            item = item.split(",")?.pop() || "";
         }
         if (item === "") {
-            return Text.empty()
+            return Text.empty();
         }
-        const buffer = Base64.toUint8Array(item)
+        const buffer = Base64.toUint8Array(item);
         if (buffer.length < 1) {
-            throw new Error("Base64 Decode Is Empty")
+            throw new Error("Base64 Decode Is Empty");
         }
-        return Text.fromUint8Array(buffer)
+        return Text.fromUint8Array(buffer);
     }
 
-
     toString(encoding?: Encoding) {
-        return this.isEmpty() ? "" : this.uint.toString(encoding)
+        return this.isEmpty() ? "" : this.uint.toString(encoding);
     }
 
     toBase64(isUrlSafe: boolean = false, dataUrlShow = false) {
         if (!dataUrlShow) {
-            return Base64.fromUint8Array(this.uint.uint8Array, isUrlSafe)
+            return Base64.fromUint8Array(this.uint.uint8Array, isUrlSafe);
         }
-        return `data:${this.mime()};base64,${Base64.fromUint8Array(this.uint.uint8Array)}`
+        return `data:${this.mime()};base64,${Base64.fromUint8Array(this.uint.uint8Array)}`;
     }
 
     toHexString() {
-        return this.toHex({type: "hex"})
+        return this.toHex({ type: "hex" });
     }
 
     toHexArray() {
-        return this.toArray().map(item => {
-            return Number(item).toString(16)
-        }) || []
+        return (
+            this.toArray().map(item => {
+                return Number(item).toString(16);
+            }) || []
+        );
     }
 
     toHex(options: hexyFormatOptions & { type: "hex" | "dump" }) {
         if (options.type === "dump") {
-            return hexy(Buffer.from(this.uint.uint8Array), options)
+            return hexy(Buffer.from(this.uint.uint8Array), options);
         }
-        return (Buffer.from(this.uint.uint8Array).toString('hex'))[options.caps === "upper" ? "toUpperCase" : 'toLowerCase']()
+        return Buffer.from(this.uint.uint8Array)
+            .toString("hex")
+            [options.caps === "upper" ? "toUpperCase" : "toLowerCase"]();
     }
 
     toUint8Array() {
-        return this.uint.uint8Array
+        return this.uint.uint8Array;
     }
 
     toBuffer() {
-        return Buffer.from(this.uint.uint8Array)
+        return Buffer.from(this.uint.uint8Array);
     }
 
     toArray() {
-        return Array.from(this.uint.uint8Array)
+        return Array.from(this.uint.uint8Array);
     }
 
     toDown() {
-        let objectUrl = window.URL.createObjectURL(new Blob([this.uint.uint8Array], {type: this.mime()}));
+        let objectUrl = window.URL.createObjectURL(new Blob([this.uint.uint8Array], { type: this.mime() }));
         let aEle = document.createElement("a");
         aEle.download = this.name();
         aEle.href = objectUrl;
@@ -164,116 +166,110 @@ class Text {
     }
 
     toDataUrl() {
-        return this.toBase64(false, true)
+        return this.toBase64(false, true);
     }
 
     name() {
         if (this.fileName) {
-            return this.fileName
+            return this.fileName;
         }
-        return `ctool-${dayjs().format('YYYY-MM-DD-HH-mm-ss')}${this.extension()}`
+        return `ctool-${dayjs().format("YYYY-MM-DD-HH-mm-ss")}${this.extension()}`;
     }
 
     extension(): string {
-        let temp = this.fileName.split('.')?.pop() || ""
+        let temp = this.fileName.split(".")?.pop() || "";
         if (temp) {
-            return `.${temp}`
+            return `.${temp}`;
         }
         if (this.isText()) {
-            return ".txt"
+            return ".txt";
         }
         const type = filetype(this.uint.localUint8Array()).filter(item => !!item.extension);
-        return type.length > 0 ? `.${type[0].extension}` : ""
+        return type.length > 0 ? `.${type[0].extension}` : "";
     }
 
     mime() {
         if (this.fileName) {
-            const ext = this.fileName.split('.')?.pop() || ""
+            const ext = this.fileName.split(".")?.pop() || "";
             if (ext && extMimeList.has(ext)) {
-                return extMimeList.get(ext)
+                return extMimeList.get(ext);
             }
         }
         const type = filetype(this.uint.localUint8Array()).filter(item => !!item.mime);
         if (type.length > 0 && type[0].mime) {
-            return type[0].mime
+            return type[0].mime;
         }
-        return this.isText() ? "text/plain" : "application/octet-stream"
+        return this.isText() ? "text/plain" : "application/octet-stream";
     }
 
     isImage(): boolean {
-        return !this.isEmpty() && [".jpg", ".jpeg", ".png", ".gif", ".webp", ".ico", ".bmp"].includes(this.extension())
+        return !this.isEmpty() && [".jpg", ".jpeg", ".png", ".gif", ".webp", ".ico", ".bmp"].includes(this.extension());
     }
 
     isText() {
-        return !this.isEmpty() && isText("", Buffer.from(this.uint.localUint8Array()))
+        return !this.isEmpty() && isText("", Buffer.from(this.uint.localUint8Array()));
     }
 
     isBinary() {
-        return !this.isEmpty() && isBinary("", Buffer.from(this.uint.localUint8Array()))
+        return !this.isEmpty() && isBinary("", Buffer.from(this.uint.localUint8Array()));
     }
 
     isAscii() {
         if (this.isEmpty()) {
-            return false
+            return false;
         }
         for (let n of this.toArray()) {
             if (n > 127) {
-                return false
+                return false;
             }
         }
-        return true
+        return true;
     }
 
     isEmpty() {
-        return this.uint.length < 1
+        return this.uint.length < 1;
     }
 
     isError() {
-        return this._isError
+        return this._isError;
     }
 
     setExtension(extension: string) {
-        this.fileName = `ctool-${dayjs().format('YYYY-MM-DD-HH-mm-ss')}${extension}`
-        return this
+        this.fileName = `ctool-${dayjs().format("YYYY-MM-DD-HH-mm-ss")}${extension}`;
+        return this;
     }
 
     setFileName(name: string) {
-        this.fileName = name
-        return this
+        this.fileName = name;
+        return this;
     }
 
     encoding() {
-        return this.uint.encoding
+        return this.uint.encoding;
     }
 
     analyseEncoding() {
-        return this.uint.analyseEncoding()
+        return this.uint.analyseEncoding();
     }
 
     async calculateImageSize() {
-        return new Promise<boolean>((resolve) => {
-            this.width = 0;
-            this.height = 0;
+        return new Promise<boolean>(resolve => {
             if (!this.isImage()) {
-                resolve(false)
+                resolve(false);
             }
             const img = new Image();
             img.onload = () => {
-                this.width = Math.ceil(img.width);
-                this.height = Math.ceil(img.height);
-                resolve(true)
-            }
-            img.onerror = () => resolve(false)
+                if (this.width === undefined && this.height === undefined) {
+                    this.width = Math.ceil(img.width);
+                    this.height = Math.ceil(img.height);
+                    this.imageSizeString = `${this.width} x ${this.height}`;
+                }
+                resolve(true);
+            };
+            img.onerror = () => resolve(false);
             img.src = this.toDataUrl();
-        })
-    }
-
-    get imageSizeString() {
-        if (!this.width || !this.height) {
-            return ""
-        }
-        return `${this.width}x${this.height}`
+        });
     }
 }
 
-export default Text
+export default Text;
