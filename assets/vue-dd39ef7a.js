85ce85f6dacd43a21df0bab223a3f07dadadfd51
baiy <baiy@users.noreply.github.com>
2023-09-01 08:08:35 +0000

    deploy: 5f1633c9d4548fc955cb35f452f19add43323c22

---
+++ assets/vue-dd39ef7a.js
@@ -0,0 +1,1 @@
+import{p as t}from"./standalone-e4bf77fb.js";import{p as r}from"./parser-html-7090c283.js";import{B as e}from"./base-f0edccdd.js";import"./tool-be31916e.js";import"./modulepreload-polyfill-3cfb730f.js";const m=new class extends e{async beautify(){return t.format(this.code,{plugins:[r],parser:"vue",tabWidth:this.getOptionValue("tab",4)})}};export{m as formatter};
