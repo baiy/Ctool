8b6007ab0715cc595ccd3bbc1f579885e36c445d
baiy <wo@baiy.org>
2024-09-09 18:58:03 +0800

    更新依赖

--- packages/ctool-core/src/tools/jwt/Jwt.vue
+++ packages/ctool-core/src/tools/jwt/Jwt.vue
@@ -2,11 +2,11 @@
     <HeightResize v-slot="{small,large}" :reduce="5">
         <Align direction="vertical">
             <Display>
-                <Textarea v-model="action.current.input" :height="small" :placeholder="$t('main_ui_input')"/>
+                <Textarea v-model="action.current.input" :height="small" :placeholder="$t('main_ui_input')" />
                 <template #extra>
                     <Align>
-                        <Bool border :size="'small'" v-model="action.current.header" label="header"/>
-                        <Bool border :size="'small'" v-model="action.current.payload" label="payload"/>
+                        <Bool border :size="'small'" v-model="action.current.header" label="header" />
+                        <Bool border :size="'small'" v-model="action.current.payload" label="payload" />
                     </Align>
                 </template>
             </Display>
@@ -23,33 +23,33 @@
 </template>
 
 <script lang="ts" setup>
-import {initialize, useAction} from "@/store/action";
-import jwtDecode from "jwt-decode"
+import { initialize, useAction } from "@/store/action";
+import { jwtDecode } from "jwt-decode";
 import Serialize from "@/helper/serialize";
-import {createSerializeOutput as serializeGetOutput} from "@/components/serialize";
+import { createSerializeOutput as serializeGetOutput } from "@/components/serialize";
 
 const action = useAction(await initialize({
     input: "",
     header: false,
     payload: true,
-    outputOption: serializeGetOutput('json'),
-}, {paste: false}))
+    outputOption: serializeGetOutput("json"),
+}, { paste: false }));
 
 const outputSerialize: Serialize = $computed(() => {
     if (!action.current.input.trim()) {
-        return Serialize.empty()
+        return Serialize.empty();
     }
     try {
         let data: Record<string, any> = {};
         if (action.current.header) {
-            data.header = jwtDecode(action.current.input, {header: true})
+            data.header = jwtDecode(action.current.input, { header: true });
         }
         if (action.current.payload) {
-            data.payload = jwtDecode(action.current.input)
+            data.payload = jwtDecode(action.current.input);
         }
-        return Serialize.formObject(data)
+        return Serialize.formObject(data);
     } catch (e) {
-        return Serialize.fromError($error(e))
+        return Serialize.fromError($error(e));
     }
-})
+});
 </script>
